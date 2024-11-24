6c6f41c187d5f03082b57c197965748c0ed01dbc
GyuhaWang <ethanleast@gmail.com>
2024-11-24 18:54:06 +0900

    UPDATE server url

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 const serverAddress = import.meta.env.VITE_SERVER_URL;
 const portNumber = 3001;
-const url = `//${serverAddress}:${portNumber}`;
+const url = `http//${serverAddress}:${portNumber}`;
 export interface SocketState {
 	connect: boolean;
 }
